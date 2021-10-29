35242d3696c48cea022f5823398afd207af13704
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-10-29 10:49:21 +0200

    add test for showing the right BMI value

--- cypress/integration/userCanCheckTheirBMI.feature.js
+++ cypress/integration/userCanCheckTheirBMI.feature.js
@@ -1,9 +1,12 @@
 describe('User can check their Bmi', () => {
-	it('user can enter weight and height', () => {
+	it('is possible to enter weight and height', () => {
 		cy.visit('http://localhost:3474')
 		cy.contains('BMI Calculator')
         cy.get("input[id=weight]").type("56")
         cy.get("#height").type("1.61")
         cy.get("#Calculate").click()
 	})
+    it('shows correct BMI value', () => {
+        cy.get("#results").should("contain", "21.60")
+    });
 })
